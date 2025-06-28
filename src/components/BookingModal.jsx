54abed5d56fc7e5a4f1c8932623b629817735f6c
ayomide-23 <aderintoayomide204@gmail.com>
2025-06-28 23:37:45 +0100

    updates

--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -44,13 +44,13 @@ const BookingModal = ({ slot, onClose, user }) => {
 
    const handlePaystackSuccess = async (reference) => {
     try {
-      const res = await fetch("http://easypark.atwebpages.com/update_status.php", {
+      const res = await fetch("https://easypark.atwebpages.com/update_status.php", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams({ slot_number: slot.slot_number }),
       });
       
-   await fetch("http://easypark.atwebpages.com/store_bookings.php", {
+   await fetch("https://easypark.atwebpages.com/store_bookings.php", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams({
